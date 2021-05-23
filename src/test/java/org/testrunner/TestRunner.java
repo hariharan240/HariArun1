a6465d25bba9ad760bdc4b0b0cb88c779b214014
Hariharan <hari.haran240@gmail.com>
2021-05-23 12:45:47 +0530

    Done

--- src/test/java/org/testrunner/TestRunner.java
+++ src/test/java/org/testrunner/TestRunner.java
@@ -6,7 +6,10 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src\\test\\resources")
+@CucumberOptions(features = "src\\test\\resources", glue = { "org.stepdefinition" })
 public class TestRunner {
+	private void sysout() {
+		// TODO Auto-generated method stub
 
+	}
 }
